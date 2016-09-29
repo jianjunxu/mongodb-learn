71640b4ab1342698fa3379c94b8a9ac25b21234b
xujianjun <xujj@jiuxiulvxing.com>
2016-09-29 14:19:59 +0800

    add jetty plugin

---
+++ src/main/java/com/jxlx/jayden/mongo/controller/TestController.java
@@ -0,0 +1,10 @@
+package com.jxlx.jayden.mongo.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * Created by jayden on 16/9/29.
+ */
+@Controller
+public class TestController {
+}
